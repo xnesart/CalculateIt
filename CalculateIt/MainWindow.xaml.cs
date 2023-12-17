b3af3c8f25be116c0fb5f91f37121195e7105803
Сергей <xnesart@mail.ru>
2023-12-18 01:00:42 +0300

    summ is done

--- CalculateIt/MainWindow.xaml.cs
+++ CalculateIt/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CalculateIt
 {
@@ -23,6 +12,95 @@ namespace CalculateIt
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private decimal _previousNumber;
+        private string _previousOperation;
+        private decimal _allNumber;
+        private bool _IsWrite;
+
+        private void ButtonNumber_Click(object sender, RoutedEventArgs e)
+        {
+            if(CheckPreviousOperationIsEqual())
+            {
+                TextBoxMain.Text = "";
+                _previousOperation = "";
+            }
+
+            if (((Button)sender).Content == "+" || ((Button)sender).Content == "-" || ((Button)sender).Content == "/" || ((Button)sender).Content == "X")
+            {
+                _previousOperation = (string)((Button)sender).Content;
+            }
+            if ((_previousOperation == "+" || _previousOperation == "-" || _previousOperation == "/" || _previousOperation == "X") && _IsWrite == false)
+            {
+                TextBoxMain.Text = "";
+                _IsWrite = true;
+            }
+
+            TextBoxMain.Text += ((Button)sender).Content;
+
+        }
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            if (_previousOperation == "=")
+            {
+                _previousNumber = int.Parse(TextBoxMain.Text);
+                _allNumber = _previousNumber;
+                TextBoxSupport.Text = $"{TextBoxMain.Text}+";
+                
+                TextBoxMain.Text = "";
+                _previousOperation = "+";
+                
+            }
+            else
+            {
+                _previousNumber = Convert.ToInt32(TextBoxMain.Text);
+                _previousOperation = "+";
+                TextBoxSupport.Text += $"{TextBoxMain.Text}+";
+                _allNumber += _previousNumber;
+                TextBoxMain.Text = "";
+            }
+
+        }
+
+        private void ButtonEqals_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckPreviousOperationIsEqual())
+            {
+                TextBoxMain.Text = "";
+                TextBoxSupport.Text = "";
+            }
+            if (_previousOperation == "+")
+            {
+                TextBoxSupport.Text += $"{TextBoxMain.Text}=";
+            }
+            _allNumber += int.Parse(TextBoxMain.Text);
+
+            TextBoxMain.Text = _allNumber.ToString();
+            _previousOperation = "=";
+            _allNumber = 0;
+
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            _previousOperation = "";
+            _previousNumber = 0;
+            _allNumber = 0;
+            TextBoxMain.Text = "";
+            TextBoxSupport.Text = "";
+        }
+
+        private bool CheckPreviousOperationIsEqual()
+        {
+            bool result = false;
+            if(_previousOperation == "=")
+            {
+                result = true;
+            }
+
+            return result;
         }
     }
 }
